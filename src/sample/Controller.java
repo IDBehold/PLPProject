d2ff6220db42abde9fa051f84858b6fa38638881
Søren Boldsen <soren.boldsen@systematic.com>
2018-04-10 11:14:55 +0200

    vbox working with image

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import scala.BitmapOps;
+import scala.RgbBitmap;
+
+import java.awt.Color;
+
 public class Controller {
+    @FXML
+    VBox box1;
+
+    public Controller() {
+    }
+
+    @FXML
+    public void initialize() {
+        RgbBitmap rgbBitmap = new RgbBitmap(300, 275);
+
+                rgbBitmap.fill(Color.WHITE);
+                BitmapOps.midpoint(rgbBitmap, 150,150,50, Color.BLUE);
+                BitmapOps.bresenham(rgbBitmap, 100, 100, 0, 0, Color.RED);
+                Image image = SwingFXUtils.toFXImage(rgbBitmap.image(), null);
+                ImageView imageView = new ImageView();
+                imageView.setImage(image);
+
+                box1.getChildren().addAll(imageView);
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import scala.BitmapOps;
-import scala.RgbBitmap;
-
-import java.awt.*;
 
 public class Main extends Application {
 
@@ -20,17 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        RgbBitmap rgbBitmap = new RgbBitmap(300, 275);
-        BitmapOps.midpoint(rgbBitmap, 150,150,50, Color.BLUE);
-        BitmapOps.bresenham(rgbBitmap, 100, 100, 0, 0, Color.RED);
-        Image image = SwingFXUtils.toFXImage(rgbBitmap.image(), null);
-        VBox vBox = new VBox();
-        ImageView imageView = new ImageView();
-        imageView.setImage(image);
-        vBox.getChildren().addAll(imageView);
 
-        Scene scene = new Scene(root, 300, 275);
-        scene.setRoot(vBox);
+        Scene scene = new Scene(root, 800, 800);
 
         primaryStage.setScene(scene);
         primaryStage.show();
