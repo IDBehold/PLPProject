a803a45404f4ec2ed0fee31ccab7c4acc39c0f43
jacobboes <jacobboeseriksen@hotmail.com>
2018-04-17 10:34:50 +0200

    input

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,13 +9,19 @@ import scala.Draw;
 import scala.Grid;
 import scala.RgbBitmap;
 
-import java.awt.Color;
+import java.awt.*;
 
 import static scala.Draw.*;
 
 public class Controller {
     @FXML
     VBox imageContainer;
+
+    @FXML
+    TextArea inputField;
+
+    @FXML
+    Label errorMessage;
 
     public Controller() {
     }
@@ -31,17 +37,18 @@ public class Controller {
         RgbBitmap rgbBitmap = new RgbBitmap((int) imageContainer.getWidth(), (int) imageContainer.getHeight(), 50, 50, 350, 350);
         rgbBitmap.fill(Color.WHITE);
         Grid.draw(rgbBitmap, (int) imageContainer.getWidth(), (int) imageContainer.getHeight());
-
-
-        Cons shapeList = new Cons(new Line(100, 100, 200, 200), new Cons(new Line(0, 0, 150, 150), new Cons(new Rectangle(30,30,50,50), new Cons(new Rectangle(100,90, 75, 350), new Cons(new Rectangle(200,200,100,100), new Nil())))));
-        rgbBitmap = draw(rgbBitmap, Color.RED, shapeList);
-        circle(rgbBitmap, 150, 150, 100, Color.BLUE);
-        circle(rgbBitmap, 150, 150, 98, Color.BLUE);
-        circleFill(rgbBitmap, 150, 150, 75, Color.CYAN);
-        line(rgbBitmap, 50, 50, 100, 200, Color.CYAN);
         rectangle(rgbBitmap,50,50,350,350, Color.BLACK);
-        rectangleFill(rgbBitmap,300,300,200,200, Color.BLACK);
-        rectangleFill(rgbBitmap,235,235,275,275, Color.GRAY);
+
+
+//        errorMessage.setText((String) Interpreter.SplitText(inputField.getText()).getLength());
+//        Cons shapeList = new Cons(new Line(100, 100, 200, 200), new Cons(new Line(0, 0, 150, 150), new Cons(new Rectangle(30,30,50,50), new Cons(new Rectangle(100,90, 75, 350), new Cons(new Rectangle(200,200,100,100), new Nil())))));
+//        rgbBitmap = draw(rgbBitmap, Color.RED, shapeList);
+//        circle(rgbBitmap, 150, 150, 100, Color.BLUE);
+//        circle(rgbBitmap, 150, 150, 98, Color.BLUE);
+//        circleFill(rgbBitmap, 150, 150, 75, Color.CYAN);
+//        line(rgbBitmap, 50, 50, 100, 200, Color.CYAN);
+//        rectangleFill(rgbBitmap,300,300,200,200, Color.BLACK);
+//        rectangleFill(rgbBitmap,235,235,275,275, Color.GRAY);
 
 
         Image image = SwingFXUtils.toFXImage(rgbBitmap.image(), null);
