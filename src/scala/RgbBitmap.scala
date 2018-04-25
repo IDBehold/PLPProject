5abcfe7677f06f3828d61fe87f9cf5b317474c9f
Søren Boldsen <soren.boldsen@systematic.com>
2018-04-25 11:15:07 +0200

    removed piechart stuff

--- src/scala/RgbBitmap.scala
+++ src/scala/RgbBitmap.scala
@@ -3,7 +3,6 @@ package scala
 import java.awt.image.BufferedImage
 import java.awt.{Color, Font}
 
-import scala.Draw.{Base, Slice, SliceList, Slices}
 import scala.annotation.tailrec
 
 class RgbBitmap(val width: Int, val height: Int, val x0: Int, val y0: Int, val x1: Int, val y1: Int) {
@@ -42,66 +41,8 @@ class RgbBitmap(val width: Int, val height: Int, val x0: Int, val y0: Int, val x
 
   def setText(x: Int, y: Int, t: String, c: Color) = {
     val g = image.getGraphics()
-    g.setFont(new Font ("Arial Black", Font.BOLD, 10))
+    g.setFont(new Font("Arial Black", Font.BOLD, 10))
     g.setColor(c)
     g.drawString(t, x, y)
-  }
-
-
-  def pieChart(sl: List[Int]): Unit = {
-    val g = image.getGraphics()
-
-    // Sum from list of slices instead
-    def sum(ints: List[Int]): Int = {
-      @tailrec
-      def sumAccumulator(ints: List[Int], accum: Int): Int = {
-        ints match {
-          case Nil => accum
-          case x :: tail => sumAccumulator(tail, accum + x)
-        }
-      }
-      sumAccumulator(ints, 0)
-    }
-
-    def sum2(xs: List[Int]): Int = {
-      if (xs.isEmpty) 0
-      else xs.head + sum2(xs.tail)
-    }
-
-    var total = 0
-    var startAngle = 0
-    var arcAngle = 0
-    var curValue = 0
-    var n = 0
-
-    val slices = sl//List(25,25,25,25)
-    val Colors = List[Color](
-      Color.BLACK,
-      Color.BLUE,
-      Color.RED,
-      Color.YELLOW,
-      Color.CYAN,
-      Color.GREEN)
-
-    total = sum(slices)
-    println("sum Total: " + total)
-    total = 0
-    println("reset Total: " + total)
-    total = sum2(slices)
-    println("sum2 Total: " + total)
-
-    // Iterate through the list recursively?
-    for (slice: Int <- slices) {
-      drawChart(n, slice)
-      n += 1 // Assignment bad (functional programming wise)?
-    }
-
-    def drawChart(n: Int, slice: Int) = {
-      startAngle = curValue * 360 / total
-      arcAngle = slice * 360 / total
-      g.setColor(Colors(n))
-      g.fillArc(100, 100, 50, 50, startAngle, arcAngle)
-      curValue += slice
-    }
   }
 }
